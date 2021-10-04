e2cc1385f2334e383bdb35b5064e7c811a269ab2
Balakumara R <b199221@gmail.com>
2021-10-04 09:45:53 +0530

    ImportAssignmentForgotten

---
+++ ImportPackage.java
@@ -0,0 +1,13 @@
+package com.Te.Assignments.Import;
+
+import com.Te.Assignments.NestedTryCatch;
+
+public class ImportPackage {
+
+	public static void main(String[] args) {
+		
+		NestedTryCatch ref=new NestedTryCatch();
+		ref.serialization("C:\\Users\\RRB\\Desktop\\Bala.txt");
+	}
+
+}
